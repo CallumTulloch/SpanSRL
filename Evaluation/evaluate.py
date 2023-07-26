e27ec8c98a545ee7f491b4fb1f00cf79a24f7449
Callum <callumtulloch1024@gmail.com>
2023-07-26 22:46:32 +0900

    add fusionMatrix

--- Evaluation/evaluate.py
+++ Evaluation/evaluate.py
@@ -181,6 +181,59 @@ def cal_span_only_f1(xs, ys, max_length):
     return df
 
 
+def seq_fusion_matrix(xs, ys, lab2id):  
+    """混合行列の計算
+    spanが正解していてラベルを間違えたものの混合行列を計算する．
+    
+    Args:
+        xs(各文章毎のラベル結果（start, end, label）のリスト) : 予測．不必要なラベルも含まれる
+        ys(各文章毎のラベル結果（start, end, label）のリスト) : 正解．
+    
+    Returns:
+        DataFrame : 混合行列
+    """
+    for lab in ['F-A', 'F-P', 'V', 'O', 'N']:
+        try:
+            del lab2id[lab]
+        except:
+            continue
+    print(lab2id)
+    array = np.zeros(len(lab2id)*(len(lab2id)).reshape(len(lab2id), (len(lab2id))))
+    for span_pre_sent, span_ans_sent in zip(xs, ys):
+        # データ成型, 予想spanのラベルにフラグメントと述語は含まれない．
+        span_label_pre_lists = [tuple(span[:3]) for span in span_pre_sent if (span[2] != 'O') and (span[2] != 'N')]   # pre にはOが含まれる. correct_spanの時にはNも含まれる
+        span_label_ans_lists = [(span[1], span[2], span[0]) for span in span_ans_sent if (span[0] != 'V') and (span[0] != 'O')] # ans にはVが含まれる
+        for pre in span_label_pre_lists:
+            for ans in span_label_ans_lists:
+                if pre[0] == ans[0] and pre[1] == ans[1]:   # spanが正解していてラベルを間違えたものの混合行列
+                    if  pre[2] != ans[2]:
+                        array[lab2id[ans[2]]][lab2id[pre[2]]] += 1
+
+    array = array.T
+    array[2] = array[0] + array[1]
+    array = array.T
+    # 各ラベルの評価
+    df = pd.DataFrame(array, columns= list(lab2id.keys()))
+    df['precision'] = df['correct_num'] / df['predict_num']
+    #df['precision_v2'] = df['correct_num'] / (df['predict_num'] + df['missed_num'])
+    df['recall'] = df['correct_num'] / df['support']
+    df['f1'] = 2*df['precision']*df['recall'] / (df['precision'] + df['recall'])
+    #df['f1_v2'] = 2*df['precision_v2']*df['recall'] / (df['precision_v2'] + df['recall'])
+    
+    # 以下全体
+    df.loc['sum'] = df.sum()
+    if df.loc['sum', 'predict_num'] == 0:
+        df.loc['sum', 'predict_num'] = 1
+    df.loc['precision','correct_num'] = df.loc['sum', 'correct_num'] / df.loc['sum', 'predict_num']
+    #df.loc['precision_v2','correct_num'] = df.loc['sum', 'correct_num'] / (df.loc['sum', 'predict_num'] + df.loc['sum', 'missed_num'])
+    df.loc['recall','correct_num'] = df.loc['sum', 'correct_num'] / df.loc['sum', 'support']
+    df.loc['f1','correct_num'] = 2*df.loc['precision','correct_num']*df.loc['recall','correct_num'] / (df.loc['precision','correct_num'] + df.loc['recall','correct_num'])
+    df.loc['f1_macro','correct_num'] = df['f1'].sum() / len(lab2id)
+    #df.loc['f1_v2','correct_num'] = 2*df.loc['precision_v2','correct_num']*df.loc['recall','correct_num'] / (df.loc['precision_v2','correct_num'] + df.loc['recall','correct_num'])
+    df = df.round(4)
+    return df
+
+
 def get_pred_dic_lists(xs, ys, lab2id):
     pred_dic_lists = []
     match_count_list = []
